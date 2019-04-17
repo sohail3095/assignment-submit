180c9b85a99014d914c1b09682fd5bebc3c76926
Sohail Khan <sohail.khan@wdadc.com>
2019-04-17 12:14:09 +0530

    Initial commit

---
+++ assets/js/costom.js
@@ -0,0 +1,37 @@
+
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:15,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:false,
+            loop:false
+        }
+    }
+})
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.body.style.backgroundColor = "white";
+}
+
+AOS.init();
